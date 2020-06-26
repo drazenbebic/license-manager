1c4059f8c1fd742f115409185e9d86558135a937
g4m3r <contact@gsoftwarelab.com>
2020-06-26 15:21:14 +0200

    Refactoring lmfwc_duplicate

--- functions/lmfwc-core-functions.php
+++ functions/lmfwc-core-functions.php
@@ -19,20 +19,15 @@ defined('ABSPATH') || exit;
  */
 function lmfwc_duplicate($licenseKey, $licenseKeyId = null)
 {
-    $duplicate = false;
-    $hash      = apply_filters('lmfwc_hash', $licenseKey);
+    $hash = apply_filters('lmfwc_hash', $licenseKey);
 
-    // Add action
     if ($licenseKeyId === null) {
         $query = array('hash' => $hash);
 
         if (LicenseResourceRepository::instance()->findBy($query)) {
-            $duplicate = true;
+            return true;
         }
-    }
-
-    // Update action
-    elseif ($licenseKeyId !== null && is_numeric($licenseKeyId)) {
+    } elseif ($licenseKeyId !== null && is_numeric($licenseKeyId)) {
         $table = LicenseResourceRepository::instance()->getTable();
 
         $query = "
@@ -41,18 +36,16 @@ function lmfwc_duplicate($licenseKey, $licenseKeyId = null)
             FROM
                 {$table}
             WHERE
-                1=1
-                AND hash = '{$hash}'
+                hash = '{$hash}'
                 AND id NOT LIKE {$licenseKeyId}
             ;
         ";
 
         if (LicenseResourceRepository::instance()->query($query)) {
-            $duplicate = true;
+            return true;
         }
     }
-
-    return $duplicate;
+    return false;
 }
 add_filter('lmfwc_duplicate', 'lmfwc_duplicate', 10, 2);
 
